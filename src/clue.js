88967d83280a7fd23d8061f7c50e6e900cafc3bd
danieldaga <daniel@arhrestauracion.com>
2022-11-09 23:06:03 +0100

    objects created

---
+++ src/clue.js
@@ -0,0 +1,136 @@
+// ITERATION 1
+
+// Suspects Array
+
+const suspectsArray = [
+    {
+        firstName: 'dani',
+        lastName: 'García',
+        occupation: 'frontend developer',
+        age: 29,
+        description:'asd',
+        image: 'img',
+        color: 'black'
+    },
+    {
+        firstName: 'dani',
+        lastName: 'García',
+        occupation: 'frontend developer',
+        age: 29,
+        description:'asd',
+        image: 'img',
+        color: 'black'
+    },
+    {
+        firstName: 'dani',
+        lastName: 'García',
+        occupation: 'frontend developer',
+        age: 29,
+        description:'asd',
+        image: 'img',
+        color: 'black'
+    },
+    {
+        firstName: 'dani',
+        lastName: 'García',
+        occupation: 'frontend developer',
+        age: 29,
+        description:'asd',
+        image: 'img',
+        color: 'black'
+    },
+    {
+        firstName: 'dani',
+        lastName: 'García',
+        occupation: 'frontend developer',
+        age: 29,
+        description:'asd',
+        image: 'img',
+        color: 'black'
+    },
+    {
+        firstName: 'dani',
+        lastName: 'García',
+        occupation: 'frontend developer',
+        age: 29,
+        description:'asd',
+        image: 'img',
+        color: 'black'
+    }
+];
+
+// Weapons Array
+
+const weaponsArray = [
+    {
+        name: 'salom',
+        weight: 30
+    },
+    {
+        name: 'salom',
+        weight: 30
+    }, 
+    {
+        name: 'salom',
+        weight: 30
+    },
+    {
+        name: 'salom',
+        weight: 30
+    },
+    {
+        name: 'salom',
+        weight: 30
+    },
+    {
+        name: 'salom',
+        weight: 30
+    }, {
+        name: 'salom',
+        weight: 30
+    },
+    {
+        name: 'salom',
+        weight: 30
+    }, {
+        name: 'salom',
+        weight: 30
+    }
+
+];
+
+// Rooms Array
+
+const roomsArray = [
+    {name: 'salom'},
+    {name: 'salom'},
+    {name: 'salom'},
+    {name: 'salom'},
+    {name: 'salom'},
+    {name: 'salom'},
+    {name: 'salom'},
+    {name: 'salom'},
+    {name: 'salom'},
+    {name: 'salom'},
+    {name: 'salom'},
+    {name: 'salom'},
+    {name: 'salom'},
+    {name: 'salom'},
+    {name: 'bedroom'}
+
+];
+
+
+// ITERATION 2
+
+function selectRandom(arr) {
+    
+}
+
+function pickMystery() {}
+
+
+// ITERATION 3
+
+function revealMystery() {}
+
